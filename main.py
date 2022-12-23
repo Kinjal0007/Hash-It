f34a5309390256cca49875160e17573aa4b12c0c
Kinjal0007 <blackpanther.bp007@gmail.com>
2022-12-23 23:53:39 +0530

    main-dev

--- main.py
+++ main.py
@@ -1,128 +1,139 @@
 import sys
-from PyQt5.uic import loadUi
+import os
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication, QWidget
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtGui import QIcon
-from PyQt5 import QtGui
-from time import sleep
+from Custom_Widgets.Widgets import *
+from ui_interface import *
 import mysql.connector as sqltor
 
-User='user'
-def Sleep(wait):
-    sleep(wait)
+settings = QSettings()
     
     
-class WelcomeScreen(QDialog):
-    def __init__(self):
-        super(WelcomeScreen, self).__init__()
-        loadUi("welcomescreen.ui",self)
-        self.login.clicked.connect(self.gotologin)
-        self.create.clicked.connect(self.gotocreate)
-        self.setWindowIcon(QtGui.QIcon("placeholder.png"))
+# class WelcomeScreen(QDialog):
+#     def __init__(self):
+#         super(WelcomeScreen, self).__init__()
+#         loadUi("welcomescreen.ui",self)
+#         self.login.clicked.connect(self.gotologin)
+#         self.create.clicked.connect(self.gotocreate)
+#         self.setWindowIcon(QtGui.QIcon("placeholder.png"))
 
-    def gotologin(self):
-        login = LoginScreen()
-        widget.addWidget(login)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+#     def gotologin(self):
+#         login = LoginScreen()
+#         widget.addWidget(login)
+#         widget.setCurrentIndex(widget.currentIndex()+1)
         
-    def gotocreate(self):
-        create=CreateAccScreen()
-        widget.addWidget(create)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+#     def gotocreate(self):
+#         create=CreateAccScreen()
+#         widget.addWidget(create)
+#         widget.setCurrentIndex(widget.currentIndex()+1)
 
-class CreateAccScreen(QDialog):
-    def __init__(self):
-        super(CreateAccScreen,self).__init__()
-        loadUi("createacc.ui",self)
-        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.confirmpasswordfield.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.signup.clicked.connect(self.signupfunction)
-        self.setWindowIcon(QtGui.QIcon("placeholder.png"))
+# class CreateAccScreen(QDialog):
+#     def __init__(self):
+#         super(CreateAccScreen,self).__init__()
+#         loadUi("createacc.ui",self)
+#         self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+#         self.confirmpasswordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+#         self.signup.clicked.connect(self.signupfunction)
+#         self.setWindowIcon(QtGui.QIcon("placeholder.png"))
         
-    def signupfunction(self):
-        fillupprofile=FillProfileScreen()        
-        widget.addWidget(fillupprofile)
-        widget.setCurrentIndex(widget.currentIndex()+1)
+#     def signupfunction(self):
+#         fillupprofile=FillProfileScreen()        
+#         widget.addWidget(fillupprofile)
+#         widget.setCurrentIndex(widget.currentIndex()+1)
         
-        username=self.usernamefield.text()
-        password=self.passwordfield.text()
-        confirm_password=self.confirmpasswordfield.text()
-        if(len(username)==0 or len(password)==0 or len(confirm_password)==0):
-            self.error.setText("Please input all fields")
+#         username=self.usernamefield.text()
+#         password=self.passwordfield.text()
+#         confirm_password=self.confirmpasswordfield.text()
+#         if(len(username)==0 or len(password)==0 or len(confirm_password)==0):
+#             self.error.setText("Please input all fields")
             
-        elif(password!=confirm_password):
-            self.error.setText("Both the passwords are not same")
-        else:
-            mycon=sqltor.connect(host="localhost",username="root",passwd="Hello1234@",database="login_details")
-            cur=mycon.cursor()
+#         elif(password!=confirm_password):
+#             self.error.setText("Both the passwords are not same")
+#         else:
+#             mycon=sqltor.connect(host="localhost",username="root",passwd="Hello1234@",database="login_details")
+#             cur=mycon.cursor() 
             
-            user_info=[username,password]
-            query="Insert into details(email,password) values('{}','{}')".format(username,password)
-            cur.execute(query)
+#             user_info=[username,password]
+#             query="Insert into details(email,password) values('{}','{}')".format(username,password)
+#             cur.execute(query)
             
-            mycon.commit()
-            mycon.close()
-            self.success.setText("Success")
-            fillupprofile=FillProfileScreen()
-            widget.addWidget(fillupprofile)
-            widget.setCurrentIndex(widget.currentIndex()+1)
+#             mycon.commit()
+#             mycon.close()
+#             self.success.setText("Success")
+#             fillupprofile=FillProfileScreen()
+#             widget.addWidget(fillupprofile)
+#             widget.setCurrentIndex(widget.currentIndex()+1)
             
         
-class FillProfileScreen(QDialog):
-    def __init__(self):
-        super(FillProfileScreen, self).__init__()
-        loadUi("fillprofile.ui",self)
-        self.image.setPixmap(QPixmap('placeholder.png'))
+# class FillProfileScreen(QDialog):
+#     def __init__(self):
+#         super(FillProfileScreen, self).__init__()
+#         loadUi("fillprofile.ui",self)
+#         self.image.setPixmap(QPixmap('placeholder.png'))
         
 
-class LoginScreen(QDialog):
-    def __init__(self):
-        super(LoginScreen, self).__init__()
-        loadUi("login.ui",self)
-        self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
-        self.login.clicked.connect(self.loginfunction)
-        self.setWindowIcon(QtGui.QIcon("placeholder.png"))
-    def loginfunction(self):
-        user=self.emailfield.text()
-        password=self.passwordfield.text()
+# class LoginScreen(QDialog):
+#     def __init__(self):
+#         super(LoginScreen, self).__init__()
+#         loadUi("login.ui",self)
+#         self.passwordfield.setEchoMode(QtWidgets.QLineEdit.Password)
+#         self.login.clicked.connect(self.loginfunction)
+#         self.setWindowIcon(QtGui.QIcon("placeholder.png"))
+#     def loginfunction(self):
+#         user=self.emailfield.text()
+#         password=self.passwordfield.text()
         
-        if(len(user)==0 or len(password)==0):
-            self.error.setText("Please input all fields")
-        else:
-            mycon=sqltor.connect(host="localhost",user="root",passwd="Hello1234@",database="login_details")
-            cur=mycon.cursor()
-            query='SELECT password FROM details WHERE email=\''+user+"\'"
-            cur.execute(query)
-            result_pass=cur.fetchone()[0]
-            global User
-            User=user
-            if(result_pass==password):
-                self.error.setText("Successfully Logged In")
-                User=user
-                Sleep(2)
-                print("Account has been successfully logged in...")
-                mainscreen=MainScreen()
-                widget.addWidget(mainscreen)
-                widget.setCurrentIndex(widget.currentIndex()+1)
+#         if(len(user)==0 or len(password)==0):
+#             self.error.setText("Please input all fields")
+#         else:
+#             mycon=sqltor.connect(host="localhost",user="root",passwd="Hello1234@",database="login_details")
+#             cur=mycon.cursor()
+#             query='SELECT password FROM details WHERE email=\''+user+"\'"
+#             cur.execute(query)
+#             result_pass=cur.fetchone()[0]
+#             global User
+#             User=user
+#             if(result_pass==password):
+#                 self.error.setText("Successfully Logged In")
+#                 User=user
+#                 print("Account has been successfully logged in...")
+#                 mainscreen=MainScreen()
+#                 widget.addWidget(mainscreen)
+#                 widget.setCurrentIndex(widget.currentIndex()+1)
                 
-            else:
-                self.error.setText("Invalid Username or Password ")
-class MainScreen(QDialog):
-    def __init__(self):
-        super(MainScreen,self).__init__()
-        loadUi("main.ui",self)
-        global User
-        self.welcome.setText(User)
+#             else:
+#                 self.error.setText("Invalid Username or Password ")
+
+
+class MainWindow(QMainWindow):
+    def __init__(self,parent=None):
+        QMainWindow.__init__(self)
+        self.ui=Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        loadJsonStyle(self,self.ui)
+        self.show()
+        # QAppSettings.updateAppSettings(self)
+        # self.menu = QtWidgets.QMenu()
+    #     self.menu.addAction("Default-Dark", self.apply_dark_theme)
+    #     self.menu.addAction("Default-Light", self.apply_light_theme)
+    #     self.ui.themeBtn.setMenu(self.menu)
+        
+    # def apply_dark_theme(self):
+    #     settings = QSettings()
+    #     settings.setValue("THEME", "Default-Dark")
+    #     QAppSettings.updateAppSettings(self)
+
+    # def apply_light_theme(self):
+    #     settings = QSettings()
+    #     settings.setValue("THEME", "Default-Light")
+    #     QAppSettings.updateAppSettings(self)
 
 #main
-app=QApplication(sys.argv)
-welcome=WelcomeScreen()
-widget=QtWidgets.QStackedWidget()
-widget.addWidget(welcome)
-widget.setFixedHeight(800)
-widget.setFixedWidth(1200)
-widget.show()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
 try:
     sys.exit(app.exec_())
 except:
--- ui_interface.py
+++ ui_interface.py
@@ -1,27 +1,24 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'interfaceKmqGGS.ui'
+## Form generated from reading UI file 'interfacexcMxgK.ui'
 ##
-## Created by: Qt User Interface Compiler version 5.14.1
+## Created by: Qt User Interface Compiler version 5.15.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
-    QRect, QSize, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
-    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
-    QRadialGradient)
+from PySide2.QtCore import *
+from PySide2.QtGui import *
 from PySide2.QtWidgets import *
 
-import resources_rc
+import QSS_Resource
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        if MainWindow.objectName():
+        if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1238, 804)
+        MainWindow.resize(1231, 835)
         MainWindow.setLayoutDirection(Qt.LeftToRight)
         MainWindow.setStyleSheet(u"*{\n"
 "border:none;\n"
@@ -50,6 +47,9 @@ class Ui_MainWindow(object):
 "background-color:rgb(9,10,37);\n"
 "padding:10px 5px;\n"
 "border-radius:5px;\n"
+"}\n"
+"#to_login, #to_register{\n"
+"	background-color: transparent;\n"
 "}")
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
@@ -65,18 +65,18 @@ class Ui_MainWindow(object):
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.stackedWidget = QStackedWidget(self.widget)
         self.stackedWidget.setObjectName(u"stackedWidget")
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.verticalLayout_3 = QVBoxLayout(self.page)
+        self.registerPage = QWidget()
+        self.registerPage.setObjectName(u"registerPage")
+        self.verticalLayout_3 = QVBoxLayout(self.registerPage)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
-        self.label = QLabel(self.page)
+        self.label = QLabel(self.registerPage)
         self.label.setObjectName(u"label")
         self.label.setPixmap(QPixmap(u":/icons/Icon/placeholder.png"))
         self.label.setScaledContents(True)
 
         self.verticalLayout_3.addWidget(self.label)
 
-        self.label_2 = QLabel(self.page)
+        self.label_2 = QLabel(self.registerPage)
         self.label_2.setObjectName(u"label_2")
         font = QFont()
         font.setPointSize(20)
@@ -86,7 +86,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addWidget(self.label_2, 0, Qt.AlignHCenter|Qt.AlignTop)
 
-        self.label_3 = QLabel(self.page)
+        self.label_3 = QLabel(self.registerPage)
         self.label_3.setObjectName(u"label_3")
         font1 = QFont()
         font1.setPointSize(12)
@@ -98,7 +98,7 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addItem(self.verticalSpacer)
 
-        self.frame = QFrame(self.page)
+        self.frame = QFrame(self.registerPage)
         self.frame.setObjectName(u"frame")
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
@@ -117,6 +117,7 @@ class Ui_MainWindow(object):
         self.lineEdit_2 = QLineEdit(self.frame)
         self.lineEdit_2.setObjectName(u"lineEdit_2")
         self.lineEdit_2.setFont(font2)
+        self.lineEdit_2.setEchoMode(QLineEdit.PasswordEchoOnEdit)
         self.lineEdit_2.setDragEnabled(False)
         self.lineEdit_2.setReadOnly(False)
 
@@ -125,10 +126,19 @@ class Ui_MainWindow(object):
         self.lineEdit_3 = QLineEdit(self.frame)
         self.lineEdit_3.setObjectName(u"lineEdit_3")
         self.lineEdit_3.setFont(font2)
+        self.lineEdit_3.setEchoMode(QLineEdit.PasswordEchoOnEdit)
         self.lineEdit_3.setDragEnabled(False)
         self.lineEdit_3.setReadOnly(False)
 
         self.verticalLayout_4.addWidget(self.lineEdit_3)
+
+        self.checkBox = QCheckBox(self.frame)
+        self.checkBox.setObjectName(u"checkBox")
+        font3 = QFont()
+        font3.setPointSize(9)
+        self.checkBox.setFont(font3)
+
+        self.verticalLayout_4.addWidget(self.checkBox, 0, Qt.AlignHCenter)
 
 
         self.verticalLayout_3.addWidget(self.frame)
@@ -137,53 +147,45 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addItem(self.verticalSpacer_2)
 
-        self.checkBox = QCheckBox(self.page)
-        self.checkBox.setObjectName(u"checkBox")
-        font3 = QFont()
-        font3.setPointSize(9)
-        self.checkBox.setFont(font3)
+        self.registerBtn = QPushButton(self.registerPage)
+        self.registerBtn.setObjectName(u"registerBtn")
+        self.registerBtn.setFont(font2)
 
-        self.verticalLayout_3.addWidget(self.checkBox, 0, Qt.AlignHCenter)
+        self.verticalLayout_3.addWidget(self.registerBtn)
 
-        self.pushButton = QPushButton(self.page)
-        self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setFont(font2)
-
-        self.verticalLayout_3.addWidget(self.pushButton)
-
-        self.pushButton_2 = QPushButton(self.page)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.to_login = QPushButton(self.registerPage)
+        self.to_login.setObjectName(u"to_login")
         font4 = QFont()
         font4.setPointSize(10)
         font4.setUnderline(True)
-        self.pushButton_2.setFont(font4)
+        self.to_login.setFont(font4)
 
-        self.verticalLayout_3.addWidget(self.pushButton_2)
+        self.verticalLayout_3.addWidget(self.to_login)
 
-        self.label_4 = QLabel(self.page)
+        self.label_4 = QLabel(self.registerPage)
         self.label_4.setObjectName(u"label_4")
         self.label_4.setFont(font3)
 
         self.verticalLayout_3.addWidget(self.label_4, 0, Qt.AlignHCenter)
 
-        self.stackedWidget.addWidget(self.page)
-        self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
-        self.pushButton_3 = QPushButton(self.page_2)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(0, 441, 356, 41))
-        self.pushButton_3.setFont(font4)
-        self.label_5 = QLabel(self.page_2)
+        self.stackedWidget.addWidget(self.registerPage)
+        self.loginPage = QWidget()
+        self.loginPage.setObjectName(u"loginPage")
+        self.to_register = QPushButton(self.loginPage)
+        self.to_register.setObjectName(u"to_register")
+        self.to_register.setGeometry(QRect(0, 441, 356, 41))
+        self.to_register.setFont(font4)
+        self.label_5 = QLabel(self.loginPage)
         self.label_5.setObjectName(u"label_5")
-        self.label_5.setGeometry(QRect(46, 185, 264, 24))
+        self.label_5.setGeometry(QRect(50, 210, 264, 24))
         self.label_5.setFont(font1)
-        self.label_6 = QLabel(self.page_2)
+        self.label_6 = QLabel(self.loginPage)
         self.label_6.setObjectName(u"label_6")
         self.label_6.setGeometry(QRect(114, 138, 128, 40))
         self.label_6.setFont(font)
-        self.frame_2 = QFrame(self.page_2)
+        self.frame_2 = QFrame(self.loginPage)
         self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setGeometry(QRect(10, 230, 356, 131))
+        self.frame_2.setGeometry(QRect(0, 230, 371, 161))
         self.frame_2.setFrameShape(QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QFrame.Raised)
         self.verticalLayout_7 = QVBoxLayout(self.frame_2)
@@ -199,25 +201,26 @@ class Ui_MainWindow(object):
         self.lineEdit_11 = QLineEdit(self.frame_2)
         self.lineEdit_11.setObjectName(u"lineEdit_11")
         self.lineEdit_11.setFont(font2)
+        self.lineEdit_11.setEchoMode(QLineEdit.PasswordEchoOnEdit)
         self.lineEdit_11.setDragEnabled(False)
         self.lineEdit_11.setReadOnly(False)
 
         self.verticalLayout_7.addWidget(self.lineEdit_11)
 
-        self.pushButton_4 = QPushButton(self.page_2)
-        self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(0, 393, 356, 41))
-        self.pushButton_4.setFont(font2)
-        self.label_7 = QLabel(self.page_2)
+        self.loginBtn = QPushButton(self.loginPage)
+        self.loginBtn.setObjectName(u"loginBtn")
+        self.loginBtn.setGeometry(QRect(10, 380, 356, 51))
+        self.loginBtn.setFont(font2)
+        self.label_7 = QLabel(self.loginPage)
         self.label_7.setObjectName(u"label_7")
-        self.label_7.setGeometry(QRect(90, 0, 171, 131))
-        self.label_7.setPixmap(QPixmap(u":/icons/Icon/check-user-logo-icon-design-vector-22953776.jpg"))
+        self.label_7.setGeometry(QRect(110, 0, 171, 131))
+        self.label_7.setPixmap(QPixmap(u":/icons/Icon/user-check.png"))
         self.label_7.setScaledContents(True)
-        self.label_8 = QLabel(self.page_2)
+        self.label_8 = QLabel(self.loginPage)
         self.label_8.setObjectName(u"label_8")
         self.label_8.setGeometry(QRect(140, 489, 75, 18))
         self.label_8.setFont(font3)
-        self.stackedWidget.addWidget(self.page_2)
+        self.stackedWidget.addWidget(self.loginPage)
 
         self.verticalLayout_2.addWidget(self.stackedWidget)
 
@@ -243,17 +246,17 @@ class Ui_MainWindow(object):
         self.lineEdit_3.setText("")
         self.lineEdit_3.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Confirm Password", None))
         self.checkBox.setText(QCoreApplication.translate("MainWindow", u"I've read terms and conditions", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Register", None))
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Already registered? Login", None))
+        self.registerBtn.setText(QCoreApplication.translate("MainWindow", u"Register", None))
+        self.to_login.setText(QCoreApplication.translate("MainWindow", u"Already registered? Login", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"Hash It Pvt", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Not registered? Register", None))
-        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Enter Your Information Below", None))
+        self.to_register.setText(QCoreApplication.translate("MainWindow", u"Not registered? Register", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Enter Your information below", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Log In", None))
         self.lineEdit_10.setText("")
         self.lineEdit_10.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Username", None))
         self.lineEdit_11.setText("")
         self.lineEdit_11.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Password", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Login", None))
+        self.loginBtn.setText(QCoreApplication.translate("MainWindow", u"Login", None))
         self.label_7.setText("")
         self.label_8.setText(QCoreApplication.translate("MainWindow", u"Hash It Pvt", None))
     # retranslateUi
